1db8382635e0047c436f687a078168345ce3c541
Dementisten <69969999+Dementisten@users.noreply.github.com>
2021-09-13 14:28:00 +0200

    Add files via upload

---
+++ Test/Rectangle.cs
@@ -0,0 +1,12 @@
+class Rectangle : Shape{
+
+    public int Area{
+        get {return width * height;}
+    }  
+
+
+    public Triangle(int width, int height): base(width, height)
+    {
+
+    }
+}
---
+++ Test/Triangle.cs
@@ -0,0 +1,8 @@
+class Triangle : Shape{
+    public int area {get;}
+    public int circumferance {get;}
+
+    
+
+
+}
