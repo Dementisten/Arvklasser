5dc52cf1321f1678aa4a9f91815b6256d3c2f0d3
Dementisten <69969999+Dementisten@users.noreply.github.com>
2021-09-30 10:37:45 +0200

    Fixade en meny till programmet

--- Personelevanställd/Program.cs
+++ Personelevanställd/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Personelevanställd
 {
@@ -6,10 +7,56 @@ namespace Personelevanställd
     {
         static void Main(string[] args)
         {
-            Elev elev1 = new Elev("Karl", "20030607-4156", 2019, "Teknik");
-            Console.WriteLine(elev1);
-            Anställd anställd1 = new Anställd("Nils", "19750612-5214", 30000);
-            Console.WriteLine(anställd1);
+            List<Elev> elever = new List<Elev>();
+            List<Anställd> anställda = new List<Anställd>();
+
+            int meny = 0;
+            while(meny != 4){
+                Console.WriteLine("\n");
+                Console.WriteLine("1. Lägg till ny anställd.");
+                Console.WriteLine("2. Lägg till ny elev.");
+                Console.WriteLine("3. Lista upp alla personer.");
+                Console.WriteLine("4. Stäng programmet.");
+                meny = int.Parse(Console.ReadLine());
+
+                if (meny == 1)
+                {
+                    Console.Write("Namn: ");
+                    string NAMN = Console.ReadLine();
+                    Console.Write("Personnummer: ");
+                    string PNR = Console.ReadLine();
+                    Console.Write("Månadslön: ");
+                    int LÖN = int.Parse(Console.ReadLine());
+                    anställda.Add(new Anställd(NAMN, PNR, LÖN));
+                }
+                if (meny == 2)
+                {
+                    Console.Write("Namn: ");
+                    string NAMN = Console.ReadLine();
+                    Console.Write("Personnummer: ");
+                    string PNR = Console.ReadLine();
+                    Console.Write("Antagningsår: ");
+                    int YEAR = int.Parse(Console.ReadLine());
+                    Console.Write("Utbildningsprogram");
+                    string PROG = Console.ReadLine();
+                    elever.Add(new Elev(NAMN, PNR, YEAR, PROG));
+                }
+                if (meny == 3)
+                {
+                    foreach (Elev a in elever)
+                    {
+                        Console.WriteLine("===================");
+                        Console.WriteLine(a);
+                    }
+                    foreach (Anställd a in anställda)
+                    {
+                        Console.WriteLine("===================");
+                        Console.WriteLine(a);
+                    }
+                }
+
+            }
         }
     }
 }
+
